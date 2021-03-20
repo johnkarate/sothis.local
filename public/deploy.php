64024120ce0eb3dc2fccd4d8c406dcefacc29baf
David Lozano <dlozanomoreno@gmail.com>
2021-03-20 19:12:43 +0100

    Test desploy

--- public/deploy.php
+++ public/deploy.php
@@ -3,28 +3,7 @@
 // Forked from https://gist.github.com/1809044
 // Available from https://gist.github.com/nichtich/5290675#file-deploy-php
 
-// Check whether client is allowed to trigger an update
-$allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
-	'195.37.139.','193.174.' // VZG
-);
-$allowed = false;
-
-$headers = apache_request_headers();
-
-if (@$headers["X-Forwarded-For"]) {
-    $ips = explode(",",$headers["X-Forwarded-For"]);
-    $ip  = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-foreach ($allowed_ips as $allow) {
-    if (stripos($ip, $allow) !== false) {
-        $allowed = true;
-        break;
-    }
-}
+$allowed = true;
 
 if (!$allowed) {
 	header('HTTP/1.1 403 Forbidden');
@@ -37,13 +16,16 @@ flush();
 // Actually run the update
 
 $commands = array(
+	'cd /www/sothis.local/',
 	'whoami',
-	'git pull',
+	'git pull origin main',
 	'git status',
 	'git submodule sync',
 	'git submodule update',
 	'git submodule status',
     'php /www/sothis.local/bin/console doctrine:schema:update --force',
+    'php /www/sothis.local/bin/console cache:clean',
+    'php /www/sothis.local/bin/console cache:warmup',
 );
 
 $output = "\n";
