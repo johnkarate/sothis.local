97d0689f20089382fbb8491309e46818aba01911
David Lozano <dlozanomoreno@gmail.com>
2021-03-20 20:03:27 +0100

    Cambio video index.php

--- public/deploy.php
+++ public/deploy.php
@@ -3,6 +3,24 @@
 // Forked from https://gist.github.com/1809044
 // Available from https://gist.github.com/nichtich/5290675#file-deploy-php
 
+//Comprobamos que: 
+// -> Sea un post
+// User-Agent contenga Github-Hookshot
+// Existan cabeceras: 
+/**
+ * X-GitHub-Delivery: e1944062-89a7-11eb-8036-6655b8ca6a14
+ * X-GitHub-Event: push
+* X-GitHub-Hook-ID: 287693790
+* X-GitHub-Hook-Installation-Target-ID: 347040339
+* X-GitHub-Hook-Installation-Target-Type: repository
+* X-Hub-Signature: sha1=c9d7dea3455774af8cec7a6e664c2e0c6e919ed9
+* X-Hub-Signature-256: sha256=64950b08b9eb0310d08d1ec79b1a5d215543a96ccbea3d5418de58638d6b193d
+ * 
+ * repository['name'] == 'sothis.local'
+ * repository['login'] == 'johnkarate'
+ * 
+ */
+
 $allowed = true;
 
 if (!$allowed) {
